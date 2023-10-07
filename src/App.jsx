4bb132e85447fc796351a182f7547600130b9303
nicky <nickylds1958@gmail.com>
2023-10-07 01:45:30 -0600

    primera version

---
+++ src/App.jsx
@@ -0,0 +1,77 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import Navegacion from "./componentes/navegacion";
+Navegacion
+
+function App() {
+  // La variable data es la que va a almacenar los datos de "stays.json" y setData nos ayudará a guardar esos datos en esa variable. Es necesario que inicialicemos esa variable como un array vacío para evitar errores.
+  const [data, setData] = useState([]);
+  const [filtered, setFiltered] = useState ([])
+
+  // Función para traer los datos de "stays.json".
+  const getData = async () => {
+    // Esta sentencia try-catch sirve para manejar los errores que se podrían generar al importar los datos de "stays.json".
+    try {
+      const res = await fetch("stays.json");
+      const resJson = await res.json();
+      // Aquí guardamos los datos de "stays.json" en la variable data.
+      setData(resJson);
+      setFiltered(resJson);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // Este Hook te va a ejecutar la función getData cada vez que la página se renderice.
+  useEffect(() => {
+    getData();
+  }, []);
+
+  // Puedes ver la variable data en consola.
+  console.log(data);
+
+  const filtrarD = (text) =>{
+   const dataFiltered = data.filter(element => element.maxGuests >= parseInt(text))
+
+   return dataFiltered
+  }
+    
+  const manejador = (e) =>{
+    e.preventDefault()
+    const text = e.target[0].value
+    const resultfiltrado = filtrarD(text) 
+    setFiltered(resultfiltrado)
+
+  }
+
+
+  return (
+    <>
+      <Navegacion fn={manejador}  ></Navegacion>
+      <main>
+
+
+        {/* Aquí te dejo un ejemplo de cómo podrías imprimir varios elementos a la vez. */}
+        {filtered.map((el, i) => {
+          return <> 
+           <div key={i} className="tarjeta">
+            <div className="contimg">
+              <img src={el.photo} alt= {el.title} />
+            </div>
+          
+            <div className="textd">
+               <h1>{el.maxGuests}</h1>
+            </div>
+           
+
+          
+          
+          </div>
+          </> 
+        })}
+      </main >
+    </>
+  );
+}
+
+export default App;
---
+++ src/componentes/Navegacion.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export default function Navegacion(props) {
+    return (
+        <>
+            <div>
+
+                <nav class="navbar bg-body-tertiary">
+                    <div class="container-fluid">
+                        <a class="navbar-brand">Navbar</a>
+                        <form class="d-flex" role="search" onSubmit={props.fn}>
+                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                            <button class="btn btn-outline-success" type="submit">Search</button>
+                        </form>
+                    </div>
+                </nav>
+            </div>
+        </>
+    )
+}
