e4741b75cb4289ffa25a0484f6e8115e025e1b99
nicky subuyu <137240558+codeflow1958@users.noreply.github.com>
2023-10-09 13:47:44 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://<codeflow1958>.github.io/<mini-proyect>/ "
+  base: "https://codeflow1958.github.io/mini-proyect/ "
 })
